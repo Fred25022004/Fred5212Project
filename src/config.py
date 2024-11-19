f7ffc0526aa40f61963518f9f597649fa0654c69
Fred25022004 <fkung9104@gmail.com>
2024-11-19 18:14:45 +0800

    changed learning rate and weight decay values

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(CURRENT_DIR)
 
 CONFIG = {
-    'learning_rate': 0.0005,
-    'weight_decay': 1e-4,
+    'learning_rate': 0.0003,
+    'weight_decay': 0.0005,
     'batch_size': 32,
     'epochs': 90,
     'random_state': 42
--- src/models/mlp.py
+++ src/models/mlp.py
@@ -2,14 +2,15 @@ import torch
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim=197):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, 1)
-        self.dropout = nn.Dropout(0.4)
-        self.activation = nn.LeakyReLU(0.1)  # 或 self.activation = nn.GELU()
+        self.fc1 = nn.Linear(input_dim, 512)  # 256 -> 512
+        self.fc2 = nn.Linear(512, 256)   # 128 -> 256
+        self.fc3 = nn.Linear(256, 128)   # 64 -> 128
+        self.fc4 = nn.Linear(128, 1)     # 保持不变
+        
+        self.dropout = nn.Dropout(0.4)    # 暂时保持不变
+        self.activation = nn.LeakyReLU(0.1)
 
     def forward(self, x):
         x = self.activation(self.fc1(x))
